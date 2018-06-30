58d5f9629276db162c4d92bded17a7d4eced6891
Carl Egbert <egbertcarl@gmail.com>
2018-06-29 19:04:29 -0700

    fix some broken stuff and add augment fn to note

--- lib/note.spec.ts
+++ lib/note.spec.ts
@@ -3,30 +3,63 @@ import { expect } from 'chai';
 import Note, { Augmentation, NoteLetter } from './note';
 
 describe('Note', () => {
-    it('should through an exception when octave is out of bounds', () => {
+    it('should throw an exception when constructor is called with octave out of bounds', () => {
         expect(() => new Note(0, -1)).to.throw();
-        expect(() => new Note(0, -1)).to.throw();
+        expect(() => new Note(0, 9)).to.throw();
     });
 
-    it('toString returns string with no accidental when not specified', () => {
-        const n: Note = new Note(0, 1);
-        const str: string = n.toString();
-        const expected: string = `${NoteLetter[0]}1`;
-        expect(str).to.equal(expected);
+    it('should throw an exception when constructor is called with augmentation out of bounds', () => {
+        expect(() => new Note(0, 1, -3)).to.throw();
+        expect(() => new Note(0, 1, 3)).to.throw();
     });
 
-    it('toString returns string with natural when specified', () => {
-        const n: Note = new Note(0, 1);
-        const str: string = n.toString(true);
-        const expected: string = `${NoteLetter[0]}${Augmentation[0]}1`;
-        expect(str).to.equal(expected);
+    describe('#toString', () => {
+        it('returns string with no accidental when not specified', () => {
+            const n: Note = new Note(0, 1);
+            const str: string = n.toString();
+            const expected: string = `${NoteLetter[0]}1`;
+            expect(str).to.equal(expected);
+        });
+
+        it('returns string with natural when specified', () => {
+            const n: Note = new Note(0, 1);
+            const str: string = n.toString(true);
+            const expected: string = `${NoteLetter[0]}${Augmentation[0]}1`;
+            expect(str).to.equal(expected);
+        });
+
+        it('returns note with accidental', () => {
+            const n: Note = new Note(0, 1, 1);
+            const str: string = n.toString();
+            const expected: string = `${NoteLetter[0]}${Augmentation[1]}1`;
+            expect(str).to.equal(expected);
+        });
     });
 
-    it('toString returns note with accidental', () => {
-        const n: Note = new Note(0, 1, 1);
-        const str: string = n.toString();
-        const expected: string = `${NoteLetter[0]}${Augmentation[1]}1`;
-        expect(str).to.equal(expected);
+    describe('#augment', () => {
+        it('throws when a note is augmented out of bounds', () => {
+            const n = new Note(0, 1);
+            expect(() => n.augment(3)).to.throw();
+            expect(() => n.augment(-3)).to.throw();
+        });
+
+        it('augments successfully', () => {
+            const n = new Note(0, 1);
+            n.augment(2);
+            expect(n.aug).to.equal(2);
+            n.augment(-4);
+            expect(n.aug).to.equal(-2);
+        });
+
+        it('augments with chaining', () => {
+            const note = new Note(0, 1);
+            note
+                .augment(-1)
+                .augment(2)
+                .augment(1)
+                .augment(-3);
+            expect(note.aug).to.equal(-1);
+        });
     });
 });
 
--- lib/note.ts
+++ lib/note.ts
@@ -16,39 +16,45 @@ export enum Augmentation {
     '𝄪' = 2,
 }
 
-const OCTAVE_MIN: number = 0;
-const OCTAVE_MAX: number = 6;
+const OCTAVE_MIN: number = 1;
+const OCTAVE_MAX: number = 7;
+const AUG_MIN: number = -2;
+const AUG_MAX: number = 2;
 
 const validateOctave: Function = (octave: number) => {
     if (octave < OCTAVE_MIN || octave > OCTAVE_MAX) throw new Error(`Octave ${octave} out of range`);
 };
 
+const validateAugmentation: Function = (augmentation: number) => {
+    if (augmentation < AUG_MIN || augmentation > AUG_MAX) throw new Error(`Augmentation to ${augmentation} out of range`);
+}
+
 export default class Note {
-    private _letter: NoteLetter;
-    private _octave: number;
-    private _aug: Augmentation;
+    public letter: NoteLetter;
+    public octave: number;
+    public aug: Augmentation;
 
     constructor (letter: NoteLetter, octave: number, aug = 0) {
         validateOctave(octave);
-        this._letter = letter;
-        this._octave = octave;
-        this._aug = aug;
-    }
-
-    get letter () {
-        return NoteLetter[this._letter];
-    }
-
-    get aug () {
-        return Augmentation[this._aug];
-    }
-
-    get octave () {
-        return this._octave;
+        validateAugmentation(aug);
+        this.letter = letter;
+        this.octave = octave;
+        this.aug = aug;
     }
 
     toString (showNaturalSymbol = false): string {
-        const augSymbol = (showNaturalSymbol || this._aug !== 0) ? Augmentation[this._aug] : '';
-        return `${this.letter}${augSymbol}${this.octave}`;
+        const augSymbol = (showNaturalSymbol || this.aug !== 0) ? Augmentation[this.aug] : '';
+        return `${NoteLetter[this.letter]}${augSymbol}${this.octave}`;
+    }
+
+    augment (augIncrement: number): Note {
+        const newAug: number = augIncrement + this.aug;
+        validateAugmentation(newAug);
+        this.aug = newAug;
+        return this;
+    }
+
+    increment (inc: number): void {
+
     }
 }
